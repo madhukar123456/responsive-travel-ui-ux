e1cdf8d7c2423e93a886903be38a0203036f2bb4
madhukar123456 <shroudmg@gmail.com>
2023-12-25 16:21:43 +0545

    first commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,17 +13,17 @@ const Footer = () => {
           </Link>
 
           <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
-            {FOOTER_LINKS.map((columns) => (
-              <FooterColumn title={columns.title}>
-                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
-                  {columns.links.map((link) => (
-                    <Link href="/" key={link}>
-                      {link}
-                    </Link>
-                  ))}
-                </ul>
-              </FooterColumn>
-            ))}
+          {FOOTER_LINKS.map((columns, index) => (
+  <FooterColumn key={index} title={columns.title}>
+    <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+      {columns.links.map((link, linkIndex) => (
+        <Link href="/" key={linkIndex}>
+          {link}
+        </Link>
+      ))}
+    </ul>
+  </FooterColumn>
+))}
 
             <div className="flex flex-col gap-5">
               <FooterColumn title={FOOTER_CONTACT_INFO.title}>
